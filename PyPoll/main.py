8bc7a31adef18d2904ad2fe5cf5da84ed6b9cec5
colleendaily <colleen237@gmail.com>
2021-07-03 12:50:38 -0400

    Added for loop and if statements to begin appending to lists.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,8 +5,9 @@ from typing import Counter
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 #Define total votes and cadidate list variables to write to
-total_votes = []
+total_votes = 0
 candidate_list = []
+counted_votes = []
 
 #Read in election_data.csv
 with open(csvpath, encoding= 'utf-8') as election_csv:
@@ -16,12 +17,17 @@ with open(csvpath, encoding= 'utf-8') as election_csv:
 
         #Count total votes in total_votes and write unique candidate to candidate_list
         for row in csvreader:
-            total_votes.append((row[0]))
+            total_votes = total_votes + 1
 
-            if row[2] not in candidate_list:
-                   candidate_list.append(row[2])
+            unique_candidate = row[2]
+
+            if unique_candidate not in candidate_list:
+                candidate_list.append(unique_candidate)
+                counted_votes.append(1)
+
+            else:
+                candidate_index = candidate_list.index(unique_candidate)
+                
+                   
         
-        #Checking to make sure above worked
-        print(len(total_votes))
-        print(candidate_list)
 
