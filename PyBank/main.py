5135db17b3365a2c870be76de643cb479fea5e8d
colleendaily <colleen237@gmail.com>
2021-07-03 10:51:33 -0400

    Adding min and max profit variables.

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,3 +20,7 @@ with open(csvpath, encoding = 'utf-8') as budget_csv:
     for i in range(len(monthly_total)-1):
         monthly_change.append(profit[i+1]-profit[i])
     
+max_profit_increase = max(monthly_change)
+max_profit_decrease = min(monthly_change)
+max_profit_month = monthly_change.index(max(monthly_change)) + 1
+min_profit_month = monthly_change.index(min(monthly_change)) + 1 
